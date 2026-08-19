ddf2bf0da8bdb7624960353796cfaba4ed1644f5
DubberRuckky <InbaTheNerd@gmail.com>
2026-08-19 21:16:15 +0530

    made a melon move

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # latte
 A hobby game developed by ZeFrenchSeal
+
+_____
+### Credits:
+Image by <a href="https://pixabay.com/users/divexfre-51177510/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=9741345">Divexfre</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=9741345">Pixabay</a>
--- game/main.py
+++ game/main.py
@@ -5,11 +5,25 @@ pygame.init()
 screen = pygame.display.set_mode((400,400))
 pygame.display.set_caption("latte")
 
+melon = pygame.image.load(r"game\images\divexfre-red-fruit-9741345_640.png").convert()
+
 load = True
 
+clock = pygame.time.Clock()
+
+x = 0
+
 while load:
+
+    screen.blit(melon , (x,30))
+    x+=1
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             load = False
+
+    pygame.display.flip()
+
+    clock.tick(60)
 
 pygame.quit()
