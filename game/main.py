54c238629e705ef158d222fc572784a57a16fe55
DubberRuckky <InbaTheNerd@gmail.com>
2026-08-20 17:15:51 +0530

    made the melon move to a random spot on the screen

--- game/main.py
+++ game/main.py
@@ -1,4 +1,5 @@
 import pygame
+import random as rand
 
 pygame.init()
 
@@ -9,23 +10,36 @@ icon = pygame.image.load(r"game\assets\Icon.png")
 pygame.display.set_icon(icon)
 
 melon = pygame.image.load(r"game\assets\divexfre-red-fruit-9741345_640.png").convert_alpha()
-melon = pygame.transform.scale(melon , (50,50))
+melon = pygame.transform.scale(melon , (100,100))
 
 load = True
 
 clock = pygame.time.Clock()
 
-x = 0
+
+melon_w, melon_h = melon.get_size()
+screen_w, screen_h = screen.get_size()
+
+x = (screen_w + screen_h)/2
+y = (screen_w + screen_h)/2
+
+min_w_touch = 0
+min_h_touch = 0
+
+max_w_touch = screen_w - melon_w
+max_h_touch = screen_h - melon_h
 
 while load:
 
-    screen.blit(melon , (x,30))
-    x+=1
+    x = rand.randint(min_w_touch, max_w_touch)
+    y = rand.randint(min_h_touch, max_h_touch)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             load = False
 
+    #screen.fill((0, 0, 0))
+    screen.blit(melon , (x,y))
     pygame.display.flip()
 
     clock.tick(24)
